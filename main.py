79f2b0864065f788bb08d6c3da5d49e9ce5444fc
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2026-01-11 21:55:29 +0200

    Solution

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import json
 from car.models import Car
+from car.serializers import CarSerializer
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    serializer = CarSerializer(car)
+    return json.dumps(serializer.data, separators=(",", ":")).encode("utf-8")
 
 
-def deserialize_car_object(json: bytes) -> Car:
-    pass
+def deserialize_car_object(car_json: str) -> Car:
+    data = json.loads(car_json)
+    serializer = CarSerializer(data=data)
+    serializer.is_valid(raise_exception=True)
+    return serializer.save()
